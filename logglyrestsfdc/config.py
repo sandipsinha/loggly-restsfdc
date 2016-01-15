230b7daf0c588fd6f9f24aa24509ad7a6cd3bc76
Sandip Sinha <amsandip@yahoo.com>
2016-01-15 14:18:31 -0800

    Added config file

---
+++ logglyrestsfdc/config.py
@@ -0,0 +1,25 @@
+"""
+	" Copyright:    Loggly, Inc.
+	" Author:       sandip Sinha
+	" Email:        ssinha@loggly.com
+	" Last Updated: 08/24/2015
+	"
+	" Config file for loggly-sfdc package.
+	"
+	"""
+import os
+
+prod = {
+    'username': 'xxx',
+    'password': 'xxy',
+    'token'   : 'ewew',
+    'wsdl'    : 'Not known'
+}
+
+sandbox = {
+    'username': 'integration@loggly.com.sandbox99',
+    'password': 'D0ll4r$4L0gs',
+    'token'   : '2moT2dyctCujjYq76bHP66Rra',
+    'wsdl'    : 'Not needed',
+}
+
